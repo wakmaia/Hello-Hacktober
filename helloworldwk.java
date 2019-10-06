9ac66e7ea832cc3dae8db921e6702e2dbe1230e5
wakmaia <wakoleva.maia@gmail.com>
2019-10-06 16:35:18 +0200

    Created Hello World in Java

---
+++ helloworldwk.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+
+public class HelloWord {
+
+   
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World!!");
+    }
+    
+}
