532c0d0af63192933de70ba1f8ce10050d24fdc2
Thamaltw97 <43210840+Thamaltw97@users.noreply.github.com>
2019-10-12 16:53:43 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        //Hacktoberfest is best
-        //btw hello world
+        
+        //This is a java program
         System.out.println("Hello, World");
     }
 
